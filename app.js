0e17aba7580a20f1323f34d04cf779637101ebd6
Miguel Traiano <mtraiano76@gmail.com>
2022-02-04 10:05:38 -0300

    Procfile y puerto para Heroku

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require ('express');
 const path = require('path');
 const app = express();
 
-app.listen(3000);
+app.listen(process.env.PORT||3000,function(){
+console.log ("Servidor corriendo en el puerto 3000");
+})
 
 const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
